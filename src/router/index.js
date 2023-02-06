55692db004df3ee27f26ad3bd389c0e723ef1ba2
Rémi Bastide <Remi.Bastide@gmail.com>
2023-02-06 10:24:46 +0100

    Info routeur

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CategorieView from '../views/CategorieView.vue'
-import ProduitView from '../views/ProduitView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,11 +15,6 @@ const router = createRouter({
       path: '/categories',
       name: 'categories',
       component: CategorieView
-    },
-    {
-      path: '/produits',
-      name: 'produits',
-      component: ProduitView
     },
     {
       path: '/about',
